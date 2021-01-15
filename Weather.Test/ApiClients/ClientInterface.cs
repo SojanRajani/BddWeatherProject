ed971543d08b8ffc0ac85f9ca7f27b5a6baa3f6c
Sojan <sojansgithub@outlook.com>
2021-01-15 15:38:58 +0530

    Adding api clients into the project

---
+++ Weather.Test/ApiClients/ClientInterface.cs
@@ -0,0 +1,49 @@
+﻿using Bdd.Project.Test.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using OpenWeatherApi;
+using System.Configuration;
+using CurrencyConverterClient;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bdd.Project.Test.ApiClients
+{
+    public class ClientInterface
+    {
+        public static WeatherResponseModel weather;
+        public static CurrencyConversionResponse currency;
+
+        private static string AppId = ConfigurationManager.AppSettings["ApiAppId"];
+        private static string CurrencyAppId = ConfigurationManager.AppSettings["CurrencyApiAppId"];
+
+        public WeatherResponseModel GetCurrentWeather(string lat, string lon)
+        {
+            Client client = new Client(new HttpClient());
+            var response = client.CurrentWeatherDataAsync(q: "", id: "", lat: lat, lon: lon, zip: "", units: Units.Metric, lang: Lang.En, mode: Mode.Json, AppId: AppId).Result;
+            weather = new WeatherResponseModel()
+            {
+                current = new Current()
+                {
+                    temp = response.Main.Temp
+                }
+            };
+            return weather;
+        }
+
+        public CurrencyConversionResponse GetCurrencyCoverted(string baseCurrency, IEnumerable<string> Symbols)
+        {
+            CurrencyConverterClientClass client = new CurrencyConverterClientClass(new HttpClient());
+            var response = client.GetLatestAsync(baseCurrency, Symbols, CurrencyAppId).Result;
+            currency = new CurrencyConversionResponse()
+            {
+                result = response.Rates1[Symbols.FirstOrDefault()]
+            };
+            return currency;
+        }
+    }
+}
---
+++ Weather.Test/Models/CurrencyConversionResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace Bdd.Project.Test.Models
+{
+    public class Query
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public int amount { get; set; }
+    }
+
+    public class Info
+    {
+        public int timestamp { get; set; }
+        public double rate { get; set; }
+    }
+
+    public class CurrencyConversionResponse
+    {
+        public bool success { get; set; }
+        public Query query { get; set; }
+        public Info info { get; set; }
+        public string historical { get; set; }
+        public string date { get; set; }
+        public double result { get; set; }
+    }
+}
