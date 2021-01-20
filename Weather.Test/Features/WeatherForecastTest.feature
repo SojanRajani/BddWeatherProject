e9d579249cc5c48f0d65d5b5ed223afcee566a28
ustdn15 <ustdn15@USTDE06>
2021-01-20 15:38:36 +0530

    WeatherforecastBDD

---
+++ Weather.Test/Features/WeatherForecastTest.feature
@@ -0,0 +1,17 @@
+﻿Feature: WeatherForecastTest
+	As a user of Weather Website
+    I want to be able to see current temperature
+    when I enter Location
+
+
+  Scenario Outline: User sees current temperature for location of his choice
+    Given the google url <baseurl>
+    When I enter search string as : Current temperature of Location <Location>
+    Given Open Weather Api Endpoint
+    When I give Latitude <Latitude> and Longitude <Longitude> 
+    Then the current temperature shown as <Temperature>
+    Examples:
+     | Location | baseurl        | Latitude | Longitude | Temperature |
+     | Cochin   | www.google.com | 9.9312   | 76.2673   |    27       |
+     | Kerala   | www.google.com | 10.8505  | 76.2711   |    27       |
+     | London   | www.google.com | 51.5074  | 0.1278    |    7        |
---
+++ Weather.Test/Steps/WeatherForecastTestSteps.cs
@@ -0,0 +1,49 @@
+﻿using Bdd.Project.Test.ApiClients;
+using Bdd.Project.Test.Models;
+using System;
+using TechTalk.SpecFlow;
+
+namespace Weather.Test.Steps
+{
+    [Binding]
+    public class WeatherForecastTestSteps
+    {
+        private string baseurl;
+        private string Location;
+        private string Latitude;
+        private string Longitude;
+        private int temperature;
+
+        [Given(@"the google url (.*)")]
+        public void GivenTheGoogleUrl(string baseurl)
+        {
+            this.baseurl = baseurl;
+        }
+
+        [Given(@"Open Weather Api Endpoint")]
+        public void GivenOpenWeatherApiEndpoint()
+        {
+        }
+
+        [When(@"I enter search string as : Current temperature of Location (.*)")]
+        public void WhenIEnterSearchStringAsCurrentTemperatureOfLocation(string Location)
+        {
+            this.Location = Location;
+        }
+
+        [When(@"I give Latitude (.*) and Longitude (.*)")]
+        public void WhenIGiveLatitudeAndLongitude(Decimal Latitude, Decimal Longitude)
+        {
+            this.Latitude = Latitude.ToString();
+            this.Longitude = Longitude.ToString();
+        }
+
+        [Then(@"the current temperature shown as (.*)")]
+        public void ThenTheCurrentTemperatureShownAs(int temperature)
+        {
+            ClientInterface weatherApi = new ClientInterface();
+            WeatherResponseModel response = weatherApi.GetCurrentWeather(Latitude, Longitude);
+            this.temperature = Convert.ToInt32(response.current.temp);
+        }
+    }
+}
